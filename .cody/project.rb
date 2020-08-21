816f8f035774f61047265e424c78f9712c4baf7f
Eric Conde <econde@stratpoint.com>
2020-08-22 03:02:21 +0800

    updated image

--- .cody/project.rb
+++ .cody/project.rb
@@ -8,7 +8,7 @@
 
 # name("example-project-name") # recommend leaving unset and codebuild will use a conventional name
 github_url("https://github.com/eaconde/jets-api.git")
-linux_image("aws/codebuild/amazonlinux2-x86_64-standard:3.0")
+linux_image("timbru31/ruby-node:2.5")
 environment_variables(
   JETS_ENV: "test",
   # API_KEY: "ssm:/codebuild/demo/api_key" # Example of ssm parameter
