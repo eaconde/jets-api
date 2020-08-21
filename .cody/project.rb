d0642f6760411af9d5e49e34afb03ebd5541398b
Eric Conde <econde@stratpoint.com>
2020-08-22 01:52:27 +0800

    development to test

--- .cody/project.rb
+++ .cody/project.rb
@@ -10,6 +10,6 @@
 github_url("https://github.com/eaconde/jets-api.git")
 linux_image("aws/codebuild/amazonlinux2-x86_64-standard:3.0")
 environment_variables(
-  JETS_ENV: "development",
+  JETS_ENV: "test",
   # API_KEY: "ssm:/codebuild/demo/api_key" # Example of ssm parameter
 )
